94c5fc528d104c6d6844ca78e6bc55e022ecfe1b
Cheril Yogi <114796225+CY2404@users.noreply.github.com>
2024-06-22 19:40:19 -0400

    .

---
+++ constants.py
@@ -0,0 +1,8 @@
+STOCK_CONTAINER_NAME = "stockdata"
+LAYOFF_CONTAINER_NAME = "layoffdata"
+
+OVERVIEW_FILE_LOC = "stock_data_overview.csv"
+OVERVIEW_FILE_LOCATION = "/Users/qwu194/Documents/playground/playgroud_azure/test_file/overview.csv"
+
+
+STOCK_START_DATE = "2022-08-01"
